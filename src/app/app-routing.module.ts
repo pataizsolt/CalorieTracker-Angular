604e5769a3d4b97112270a7ef49d0375c5d31001
pataizsolt <pataizsolt08@gmail.com>
2022-05-06 20:58:35 +0200

    ye

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,11 @@ import { AuthGuard } from './common/services/auth.guard';
 
 const routes: Routes = [
 
-  
+
 { path: 'foodbank', loadChildren: () => import('./pages/foodbank/foodbank.module').then(m => m.FoodbankModule), canActivate: [AuthGuard]},
-  
+
 { path: 'not-found', loadChildren: () => import('./pages/not-found/not-found.module').then(m => m.NotFoundModule) },
-  
+
 { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) },
 
 { path: 'register', loadChildren: () => import('./pages/register/register.module').then(m => m.RegisterModule) },
@@ -23,14 +23,11 @@ const routes: Routes = [
   redirectTo: '/login',
   pathMatch: 'full'
 },
-
-  
-
 {
   path: '**',
   redirectTo: '/not-found'
 }
-  
+
 ];
 
 @NgModule({
